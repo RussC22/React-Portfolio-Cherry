f60ed51b01f2dd9c895082db9118cae12185bcb7
RussC22 <tavarus.cherry2@yahoo.com>
2023-04-11 11:05:11 -0400

    PortfolioUpdate

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,6 +13,31 @@ function Portfolio() {
       githubLink: "https://github.com/RussC22/Tech-Blog",
       image: "Tech-Blog.png",
     },
+    // {
+    //   name: "Text-Editor",
+    //   description:
+    //     "This text editor runs in the browser, this app will be a single-page application that meets the PWA criteria. Additionally, it features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser; the application also functions offline.",
+    //   deployedLink: "https://text-editor-russc22.herokuapp.com/",
+    //   githubLink: "https://github.com/RussC22/Text-Editor",
+    //   image: "Text_Editor.png",
+    // },
+    // {
+    //   name: "Social-Network-API",
+    //   description:
+    //     "This is an API for my social network that uses a NoSQL database",
+    //   demoVideoLink: "https://drive.google.com/file/d/1nTn_3mMy9QNPsr6BZo57RrHkIT0UfsCc/view",
+    //   githubLink: "https://github.com/RussC22/Social-Network-API",
+    //   image: "Social Network SS.png",
+    // },
+    {
+      // Example
+      name: "Tech-Blog",
+      description:
+        "An application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.",
+      deployedLink: "https://tech-blog-russ.herokuapp.com/",
+      githubLink: "https://github.com/RussC22/Tech-Blog",
+      image: "Tech-Blog.png",
+    },
   ];
   return (
     <div className="content pb-5">
