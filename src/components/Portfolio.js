b1f1e6ed2a944762a36fb22824489ea771a594eb
RussC22 <114961971+RussC22@users.noreply.github.com>
2023-04-10 20:17:12 -0400

    PortfolioPush

---
+++ src/components/Portfolio.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Project from "../Project";
+import { Container, Row, Col } from "react-bootstrap";
+function Portfolio() {
+  // name, description, deployedLink, githubLink, image {each project}
+  const projects = [
+    {
+      // Example
+      name: "Tech-Blog",
+      description:
+        "An application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.",
+      deployedLink: "https://tech-blog-russ.herokuapp.com/",
+      githubLink: "https://github.com/RussC22/Tech-Blog",
+      image: "Tech-Blog.png",
+    },
+  ];
+  return (
+    <div className="content pb-5">
+      <h2>Portfolio</h2>
+
+      <Container className="bg-light bg-opacity-50 rounded-4 shadow mt-5 mb-5">
+        <Row className="portfolio p-3">
+          {projects.map((project) => (
+            <Col xs={6} md={4} className="project-col">
+              <Project project={project} key={project.name} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Portfolio;
